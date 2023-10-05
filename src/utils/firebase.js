2cb4bb4c1cd82daf548b6c12238d0c34da8fa6b5
Aj-654 <ajinkya0654@gmail.com>
2023-10-06 00:13:59 +0530

    updated firebase config

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,13 +8,12 @@ import { getDatabase } from "firebase/database";
 // } = require("firebase/app-check");
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  apiKey: "AIzaSyCoCpgoXcxItAve9MJ5oTnnKfrTSIxXxWc",
+  authDomain: "sem5-hrms.firebaseapp.com",
+  projectId: "sem5-hrms",
+  storageBucket: "sem5-hrms.appspot.com",
+  messagingSenderId: "334753844354",
+  appId: "1:334753844354:web:280169a863a7daf3a2389b"
 };
 // Initialize Firebase
 

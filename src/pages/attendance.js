6f4dbc6f7396d76f3e723341e7bcb7aa9036744c
Aj-654 <ajinkya0654@gmail.com>
2023-10-16 10:58:52 +0530

    commit

--- src/pages/attendance.js
+++ src/pages/attendance.js
@@ -1,49 +1,53 @@
 import React, { useState } from 'react';
-import firebase from 'firebase/app';
-import 'firebase/database';
 
 function Attendance() {
-  const [punchData, setPunchData] = useState([]); // Array to store punch-in and punch-out timestamps
+  const [punchData, setPunchData] = useState([]); 
   const [location, setLocation] = useState(null);
 
   const isOfficeLocation = (latitude, longitude) => {
     // Replace these coordinates with the latitude and longitude of your office location.
-    const officeLatitude = 19.0265615;
-    const officeLongitude = 73.0203230;
+    const officeLatitude = 19.045268677161285;
+    const officeLongitude = 72.88896416938032;
     
     // Calculate the maximum allowed distance (in meters) from the office.
-    const maxDistance = 200; // Adjust this value as needed.
+    const maxDistance = 100; // Adjust this value as needed.
     
     // Calculate the distance between the current location and the office using the Haversine formula.
     const earthRadius = 6371e3; // Earth's radius in meters.
+    console.log(earthRadius)
     const lat1 = (latitude * Math.PI) / 180;
+    console.log(Math.PI);
+    console.log(lat1);
     const lat2 = (officeLatitude * Math.PI) / 180;
+    console.log(lat2);
     const deltaLat = ((officeLatitude - latitude) * Math.PI) / 180;
+    console.log(deltaLat);
     const deltaLon = ((officeLongitude - longitude) * Math.PI) / 180;
+    console.log(deltaLon);
 
     const a = Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2) +
       Math.cos(lat1) * Math.cos(lat2) *
       Math.sin(deltaLon / 2) * Math.sin(deltaLon / 2);
+      console.log(a);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    console.log(c);
     const distance = earthRadius * c;
-
+    console.log(distance);
     return distance <= maxDistance;
   };
 
   const handlePunchIn = () => {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition((position) => {
-        const { longitude,latitude } = position.coords;
-
+        const { latitude, longitude } = position.coords;
+        
         if (isOfficeLocation(latitude, longitude)) {
           const punchInTime = new Date().toLocaleString();
-          
-          // Store punch-in time in the array
+          setLocation({ latitude, longitude, punchInTime });
           setPunchData([...punchData, { punchIn: punchInTime }]);
-          alert("Punch In Recorded!");
+          alert("Attendance Recorded!");
         } else {
           alert("You are not at the office location. Punch In is not allowed.");
-          console.log(longitude,latitude,isOfficeLocation(latitude,longitude))
         }
       }, (error) => {
         console.error("Error getting location:", error);
@@ -74,26 +78,46 @@ function Attendance() {
       ) : (
         <p>Click "Punch In" to record your attendance.</p>
       )}
-      <button onClick={handlePunchIn} style={{ padding: '10px 20px', borderRadius: '8px', fontSize: '16px' }}>
+      <button
+        onClick={handlePunchIn}
+        style={{
+          padding: '10px 20px',
+          borderRadius: '8px',
+          fontSize: '16px',
+        }}
+      >
         Punch In
       </button>
-
-      <table>
+  
+      <table
+        style={{
+          width: '100%',
+          fontSize: '20px',
+          borderCollapse: 'separate',
+          borderSpacing: '0',
+          border: '1px solid #ccc',
+          borderRadius: '10px',
+        }}
+      >
         <thead>
           <tr>
-            <th>Punch In</th>
-            <th>Punch Out</th>
-            <th>Actions</th>
+            <th style={{ width: '30%', border: '1px solid #ccc' }}>Punch In</th>
+            <th style={{ width: '30%', border: '1px solid #ccc' }}>Punch Out</th>
+            <th style={{ width: '40%', border: '1px solid #ccc' }}>Actions</th>
           </tr>
         </thead>
         <tbody>
           {punchData.map((entry, index) => (
             <tr key={index}>
-              <td>{entry.punchIn}</td>
-              <td>{entry.punchOut || '-'}</td>
-              <td>
+              <td style={{ width: '30%', border: '1px solid #ccc', textAlign: 'center' }}>{entry.punchIn}</td>
+              <td style={{ width: '30%', border: '1px solid #ccc', textAlign: 'center' }}>{entry.punchOut || '-'}</td>
+              <td style={{ width: '40%', border: '1px solid #ccc', textAlign: 'center' }}>
                 {!entry.punchOut && (
-                  <button onClick={() => handlePunchOut(index)}>
+                  <button onClick={() => handlePunchOut(index)} style={{
+                    padding: '10px 20px',
+                    borderRadius: '8px',
+                    fontSize: '16px',
+                  }}>
                     Punch Out
                   </button>
                 )}
@@ -104,6 +128,7 @@ function Attendance() {
       </table>
     </div>
   );
+  
+   
 }
-
 export default Attendance;

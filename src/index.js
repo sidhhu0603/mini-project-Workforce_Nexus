5d3cf834a6e62dd60fc1f69d097da77224b11e3e
sidhhu0603 <d2021.siddhant.bhoi@ves.ac.in>
2024-02-01 01:31:41 +0530

    added script for analytics

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "simplebar/src/simplebar.css";
 import App from "./App";
 import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
+<script defer data-domain="lucent-phoenix-eea33f.netlify.app/login" src="https://plausible.io/js/script.js"></script>
 ReactDOM.render(
   <HelmetProvider>
     <BrowserRouter>
